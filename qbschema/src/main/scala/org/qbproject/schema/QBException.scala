5111abea00de1b8131af706cb8b67d0647cabc21
Edgar Mueller <edgar.m.mueller@gmail.com>
2014-12-04 21:52:30 +0100

    Included Validation in QBMongoDefautlCollection & made QBException a regular exception

--- qbschema/src/main/scala/org/qbproject/schema/QBException.scala
+++ qbschema/src/main/scala/org/qbproject/schema/QBException.scala
@@ -3,13 +3,13 @@ package org.qbproject.schema
 import play.api.libs.json._
 
 
-class QBException(message: String, underlying: Throwable) extends RuntimeException(message, underlying) {
+class QBException(message: String, underlying: Throwable) extends Exception(message, underlying) {
   def this() = this("", null)
   def this(message: String) = this(message, null)
   def this(underlying: Throwable) = this(null, underlying)
 }
 
-class QBValidationException(message: String, underlying: Throwable, error: Option[JsError] = None) extends QBException(message, underlying) {
+case class QBValidationException(message: String, underlying: Throwable, error: Option[JsError] = None) extends QBException(message, underlying) {
   def this() = this("", null)
   def this(message: String) = this(message, null)
   def this(underlying: Throwable) = this(null, underlying)
